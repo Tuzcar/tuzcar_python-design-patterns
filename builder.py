cee22f923fb481eb94a8fffdcd23f580dfb31073
Andrew I M <65468553+Tuzcar@users.noreply.github.com>
2025-01-03 00:21:09 +0000

    Finished example

--- builder.py
+++ builder.py
@@ -4,10 +4,13 @@ class Director():
         self._builder = builder 
         
     def construct_car(self):
-        
-        
+        self._builder.create_new_car()
+        self._builder.add_model()
+        self._builder.add_tires()
+        self._builder.add_engine()
+
     def get_car(self):
-        
+        return self._builder.car
         
         
  
@@ -30,6 +33,9 @@ class SkyLarkBuilder(Builder):
     def add_tires(self):
         self.car.tires = "Regular tires"
 
+    def add_engine(self):
+        self.car.engine = "Turbo engine"
+
     
 
 
@@ -43,4 +49,9 @@ class Car():
     def __str__(self):
         return '{} | {} | {}'.format(self.model, self.tires, self.engine)
 
+builder  = SkyLarkBuilder()
+director = Director(builder)
+director.construct_car()
+car = director.get_car()
 
+print(car)
